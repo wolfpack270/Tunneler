8d47da6b753e43bc5b7271bb1c6fab8a09e37ff0
wolfpack270 <austinmcwhirter@gmail.com>
2018-10-12 14:15:48 -0500

    Tab keys change focus, gui improved

--- Tunneler/TunnGui.py
+++ Tunneler/TunnGui.py
@@ -1,5 +1,6 @@
 import tkinter
 from tkinter import ttk
+from tkinter.scrolledtext import ScrolledText
 import Tunneler
 
 menu = ['local        |   self->self->redirector->dest','remote    |   {any}->origin->self->dest','dynamic |   self->self->redirecter->{any}']
@@ -15,10 +16,10 @@ class TunnGui(object):
         
         self.rootFrame = tkinter.PanedWindow(orient=tkinter.HORIZONTAL,borderwidth=3)
         
+        
         self.createLeftSide()
         self.createRightSide()
-        
-        self.rootFrame.grid(row=0,column=0)
+        self.rootFrame.grid(row=0,column=0,sticky=tkinter.NSEW)
         
         # Because we have a grid layout, in order for user resizing to work we have to configure row/column weights
         rows,cols = self.top.grid_size()
@@ -29,22 +30,41 @@ class TunnGui(object):
         
         
         self.top.mainloop()
+        
+    def _config_text(self,event):
+        ind = self.dropdown.current()
+        if ind == -1:
+            for i in self.entries[1:]:
+                self.entries.config(state=tkinter.DISABLED,background='lightgrey')
+        if ind==0:
+            self.entries[1].config(state=tkinter.DISABLED,background='lightgrey')
+            for i in self.entries[2:]:
+                i.config(state=tkinter.NORMAL,background='white')
+        elif ind==1:
+            self.entries[3].config(state=tkinter.DISABLED,background='lightgrey')
+            for i in self.entries[1:3]:
+                i.config(state=tkinter.NORMAL,background='white')
+        elif ind==2:
+            self.entries[3].config(state=tkinter.NORMAL,background='white')
+            for i in self.entries[1:3]:
+                i.config(state=tkinter.DISABLED,background='lightgrey')
+            pass
     
     def createLeftSide(self):
         left_frame = tkinter.PanedWindow(self.top,orient=tkinter.VERTICAL,borderwidth=3)
-        # Currently text input in the first column can be set up with createInput. This may change.
         
-        self.createInput(left_frame,"User:       ")
-        self.createInput(left_frame,"Src ip:     ")
-        self.createInput(left_frame,"Dest ip:  ") 
-        self.createInput(left_frame,"Redir ip: ")
+        self.createInput(left_frame,"User:         ",disabled=False)
+        self.createInput(left_frame,"Origin ip: ")
+        self.createInput(left_frame,"Dest ip:    ") 
+        self.createInput(left_frame,"Redir ip:   ")
         
         horiz_frame = tkinter.Frame()
         creator = tkinter.Button(horiz_frame,command=self.createTunnel,text="Create!",width=10,height=2)
         creator.grid(row=0,column=0,pady=(10,0))
         left_frame.add(horiz_frame)
-        self.rootFrame.add(left_frame,width=300)
+        self.rootFrame.add(left_frame,padx=5,width=300,stretch="always")
         
+   
     def createRightSide(self):
         # Paned Window is a resizeable frame that can be "packed" by adding frames/child widgets. They get added in the orient direction.
         right_frame = tkinter.PanedWindow(self.top,orient=tkinter.VERTICAL,borderwidth=3)
@@ -56,24 +76,19 @@ class TunnGui(object):
         label.grid(row=0,column=0)
     
         self.choice = tkinter.StringVar()
-        self.dropdown = ttk.Combobox(right_horiz_frame,values=menu,textvariable=self.choice,state='readonly',justify=tkinter.LEFT,width=30)
+        self.dropdown = ttk.Combobox(right_horiz_frame,values=menu,textvariable=self.choice,state='readonly',justify=tkinter.LEFT,width=40)
+        self.dropdown.bind("<<ComboboxSelected>>", self._config_text)
         self.dropdown.grid(row=0,column=1)
    
-        right_frame.add(right_horiz_frame,padx=10,pady=10,stretch="always")
+        right_frame.add(right_horiz_frame)
+        self.text = ScrolledText(right_frame,wrap=tkinter.WORD,height=10,state='disabled')
+        right_frame.add(self.text,stretch='always')
+        
     
-    
-        # Currently self.text can be used for feedback to the user. Eventually this should be split to show feedback as well as contain
-        # a list of tunnels
-    
-        self.text = tkinter.Text(right_frame,wrap=tkinter.WORD,width=60,height=10,state='disabled')
-        right_frame.add(self.text,padx=10,pady=10,stretch="always")
-    
-        self.text_button = tkinter.Button(text="Clear",command=lambda: self.clear(self.text))
-        right_horiz_frame_2.add(self.text_button,height=30,width=50,padx=20)
-        right_horiz_frame_2.add(tkinter.Frame(width=5))
+        self.text_button = tkinter.Button(right_horiz_frame_2,text="Clear",command=lambda: self.clear(self.text))
+        self.text_button.grid(row=0,column=0,padx=(10,0))
         right_frame.add(right_horiz_frame_2)
-        self.rootFrame.add(right_frame,width=400)
-    
+        self.rootFrame.add(right_frame,padx=10,pady=10, width=400,stretch="always")
     
         
     # Since we don't want user to be able to control the feedback textbox, we have to enable/disable
@@ -93,6 +108,17 @@ class TunnGui(object):
         '''Method for returning the contents of a Text widget'''
         val = entry.get("1.0",tkinter.END)
         return val
+    def focus_next_window(self,event):
+        '''Method to make tab change textboxes'''
+        event.widget.tk_focusNext().focus()
+        return("break")
+    def focus_prev_window(self,event):
+        '''Method to make tab change textboxes'''
+        event.widget.tk_focusPrev().focus()
+        return("break")        
+    def enter_createTunnel(self,event):
+        self.createTunnel()
+        return("break")
     
     
     def createTunnel(self):
@@ -110,14 +136,16 @@ class TunnGui(object):
                 cmd = tunnel.establish() # Eventually we want to return the pid of the process and track tunnel start/end instead of command
                 self.setText(self.text,cmd)
         except Exception as e:
-            print(e)
-            self.setText(self.text,"Something went wrong")
+            self.setText(self.text,str(e))
         
-    def createInput(self,frame,label):
+    def createInput(self,frame,label,disabled=True):
         '''Creates a text box with a label in a specified frame'''
         left_horiz_frame = tkinter.PanedWindow(orient=tkinter.HORIZONTAL)
         label = tkinter.Label(text=label,justify=tkinter.LEFT)
-        text = tkinter.Text(wrap=tkinter.WORD,height=2,width=20)
+        text = tkinter.Text(wrap=tkinter.WORD,height=1,width=20,state=(tkinter.DISABLED if disabled else tkinter.NORMAL),background=('lightgrey' if disabled else 'white'))
+        text.bind("<Tab>", self.focus_next_window)
+        text.bind("<Shift-Tab>",self.focus_prev_window)
+        text.bind("<Return>",self.enter_createTunnel)
         self.entries.append(text)
         left_horiz_frame.add(label,pady=10)
         left_horiz_frame.add(text,pady=10)
